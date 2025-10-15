7ced97386c3dde4520042a489137d5c0c0831c2c
hamsoool <hanslacuesta@gmail.com>
2025-10-15 11:12:07 +0800

    checkpoint

---
+++ src/app/contact/button.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function ButtonComponent() {
+     return <button onClick={() => alert("hello")}>Click Me</button>;
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,16 @@
-export default function Contact() {
+import ButtonComponent from "./button";
 
-    return <div>Contact Page</div>
+export default async function Contact() {
+    console.log("Contact page loaded in the server or client");
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await response.json()
+    console.log(posts);
+
+
+    return (
+        <div>
+          <ButtonComponent />
+        </div>
+    );
 }
