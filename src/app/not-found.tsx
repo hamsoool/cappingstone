ade853b1998fe0a43460677f5188bde718fefb7f
hamsoool <hanslacuesta@gmail.com>
2025-10-15 13:25:02 +0800

    2nd checkpoint

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFoundPage() {
+    return (
+        <div style = {{ textAlign: "center", padding: "50px" }}>
+        <h1 style = {{ fontSize: "3rem", color: "#ff6347"}} >
+        404 - Page not Found </h1>
+        <p style ={{fontSize: "1.2rem"}} >
+        Oops! The page you are looking for does not exist.
+        </p>
+        <a href="/" style={{ color: "#42ffca", textDecoration: "underline" }}>
+        Go Back to Home
+        </a>
+        </div>
+    );
+}
