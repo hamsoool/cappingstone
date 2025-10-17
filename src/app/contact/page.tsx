f489492548857556d6636a5a6e459ec701b2678a
hamsoool <hanslacuesta@gmail.com>
2025-10-17 10:13:18 +0800

    final push

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,17 @@
+import { Metadata } from "next";
+
 import ButtonComponent from "./button";
+
+
+export const metadata: Metadata = {
+  title: "About Capppingstone",
+  description: "Lot of description here",
+  keywords: "about, cap, sol, next",
+  twitter: {
+    card: "summary_large_image",
+    title: "About Cappingstone",
+  },
+};
 
 export default async function Contact() {
     console.log("Contact page loaded in the server or client");
